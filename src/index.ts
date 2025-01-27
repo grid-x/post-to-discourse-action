c28047364b29c4de8ec45e38067641fda660fcc9
Wolfgang Werner <mail@wolfgang-werner.net>
2025-01-27 17:22:17 +0100

    feat: Allow for replacing a single post's content instead of only adding replies.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import * as core from '@actions/core'
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 const discourseUrl: string = core.getInput('discourse_url')
 const discourseTopicId: string = core.getInput('discourse_topic_id')
+const discoursePostId: string = core.getInput('discourse_post_id')
 const discourseApiKey: string = core.getInput('discourse_api_key')
 const discourseUser: string = core.getInput('discourse_user')
 const commit: string = core.getInput('github_sha')
@@ -17,6 +18,7 @@ const contentFile: string = core.getInput('content_file')
 run(
   discourseUrl,
   discourseTopicId,
+  discoursePostId,
   discourseApiKey,
   discourseUser,
   commit,
--- src/test.ts
+++ src/test.ts
@@ -4,22 +4,26 @@ import { execSync } from 'node:child_process'
 /*
 export DISCOURSE_URL=community.developer.gridx.de
 export DISCOURSE_TOPIC_ID=<n>
+export DISCOURSE_POST_ID=<n>
 export DISCOURSE_API_KEY=...
 export DISCOURSE_USER=wwerner
 */
 
 const discourseUrl: string = process.env.DISCOURSE_URL ?? ''
 const discourseTopicId: string = process.env.DISCOURSE_TOPIC_ID ?? ''
+const discoursePostId: string = process.env.DISCOURSE_POST_ID ?? ''
 const discourseApiKey: string = process.env.DISCOURSE_API_KEY ?? ''
 const discourseUser: string = process.env.DISCOURSE_USER ?? ''
 const commit: string = execSync('git rev-parse --short HEAD').toString()
 
-const content = ''
+const content = 'I am the replacement!'
 const contentFile = './test-post.md'
+
 
 run(
   discourseUrl,
   discourseTopicId,
+  "",//discoursePostId,
   discourseApiKey,
   discourseUser,
   commit,
